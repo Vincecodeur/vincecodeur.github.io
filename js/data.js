ff822855862132632e96fbd005769df282d7c0bf
Vincent <48592098+Vincecodeur@users.noreply.github.com>
2021-05-13 11:51:44 +0200

    upload portfolio

---
+++ js/data.js
@@ -0,0 +1,50 @@
+const data =[
+    {
+        id: 1,
+        date : "04/04/2021",
+        imagePath: "images/portfolio/1.jpg ",
+        title : "Javascript",
+        dataCategory : "Javascript",
+        link: "A DEFINIR", // lien github ou codePen
+        description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas.",
+        tags: "",
+    },
+    {
+        id: 1,
+        date : "04/04/2021",
+        imagePath: "images/portfolio/1.jpg ",
+        title : "Javascript",
+        dataCategory : "Javascript",
+        link: "A DEFINIR", // lien github ou codePen
+        description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas.",
+        tags: "",
+    },
+    {
+        id: 1,
+        date : "04/04/2021",
+        imagePath: "images/portfolio/1.jpg ",
+        title : "Javascript",
+        dataCategory : "Javascript",
+        link: "A DEFINIR", // lien github ou codePen
+        description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas.",
+        tags: "",
+    },
+    {
+        id: 1,
+        date : "04/04/2021",
+        imagePath: "images/portfolio/1.jpg ",
+        title : "Javascript",
+        dataCategory : "Javascript",
+        link: "A DEFINIR", // lien github ou codePen
+        description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas.",
+        tags: "",
+    },
+]
+
+
+
+
+
+
+
+export {data} 
